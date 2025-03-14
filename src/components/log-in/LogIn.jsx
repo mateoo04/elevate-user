a813114927c371b0692fce80e64f7917967a3080
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2025-03-14 14:53:29 +0100

    add Log in form validation

--- src/components/log-in/LogIn.jsx
+++ src/components/log-in/LogIn.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { useNavigate, Link } from 'react-router-dom';
 import Header from '../header/Header';
 
+const logInSchema = z.object({
+  email: z.string().email('Invalid email format'),
+  password: z
+    .string()
+    .min(8, 'Password must be at least 8 characters long')
+    .regex(/[A-Z]/, 'Must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Must contain at least one lowercase letter')
+    .regex(/\d/, 'Must contain at least one number'),
+});
+
 export default function LogIn() {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({ email: '', password: '' });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(logInSchema) });
 
   const logInUrl = import.meta.env.VITE_API_BASE_URL + '/auth/log-in';
 
-  const handleLogIn = async (e) => {
-    e.preventDefault();
-
+  const handleLogIn = async (data) => {
     try {
       const response = await fetch(logInUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(data),
       });
 
       if (!response.ok) throw new Error('Failed to fetch data');
@@ -36,22 +50,23 @@ export default function LogIn() {
     }
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
   return (
     <>
       <Header hideLogInButton={true}></Header>
       <main>
-        <form onSubmit={handleLogIn}>
+        <ul>
+          {Object.values(errors).map((error) => {
+            return <li>{error.message}</li>;
+          })}
+        </ul>
+        <form onSubmit={handleSubmit(handleLogIn)}>
           <label htmlFor='email'>
             Email
-            <input type='email' name='email' onChange={handleChange} />
+            <input type='email' name='email' {...register('email')} />
           </label>
           <label htmlFor='password'>
             Password
-            <input type='password' name='password' onChange={handleChange} />
+            <input type='password' name='password' {...register('password')} />
           </label>
           <input type='submit' value='Log in' />
         </form>
